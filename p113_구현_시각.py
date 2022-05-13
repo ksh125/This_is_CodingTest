6987ad148f208d3909ae18596de0a058fe8be0e2
ksh125 <rlatjdgns4238@gmail.com>
2022-05-13 20:25:05 +0900

    upload

---
+++ p113_구현_시각.py
@@ -0,0 +1,12 @@
+# H(hour) 입력받기
+h = int(input())
+
+count = 0
+for i in range(h + 1):
+    for j in range(60):
+        for k in range(60):
+            # 매 시간마다 3이 있다면 카운트 +1
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+                
+print(count)
