8399b424aa4a0dd316b7b088cdbe9ad1813053f5
ksh125 <rlatjdgns4238@gmail.com>
2022-05-20 20:34:09 +0900

    upload

--- 백준/b3_피보나치_수_5.py
+++ 백준/b3_피보나치_수_5.py
@@ -6,3 +6,13 @@ def fib(n):
 
 n = int(input())
 print(fib(n))
+# 0 1 2 3 4 5 6 7  8
+# 0 1 1 2 3 5 8 13 21
+def dfs(n):
+    if n < 2 :
+        return n
+    
+    return dfs(n-1) + dfs(n-2)
+
+
+print(dfs(5))
