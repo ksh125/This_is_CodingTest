f31da82f7324adad43956bdf9b9b3253a78485c1
ksh125 <rlatjdgns4238@gmail.com>
2022-05-21 12:21:30 +0900

    upload

--- 책 코드/p178_위에서_아래로.py
+++ 책 코드/p178_위에서_아래로.py
@@ -4,7 +4,7 @@ array = []
 for i in range(n):
     array.append(int(input()))
     
-array = sorted(array, reverse=True)
+array = sorted(array, reverse=True) 
 
 for i in array:
     print(i, end=' ')
