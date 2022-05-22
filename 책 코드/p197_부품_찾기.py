29f6e0c6fffdc576a5b4ffc8668fbc011d1841dd
ksh125 <rlatjdgns4238@gmail.com>
2022-05-22 12:05:32 +0900

    upload

--- 책 코드/p197_부품_찾기.py
+++ 책 코드/p197_부품_찾기.py
@@ -1,20 +1,20 @@
-# 정수 n 입력
-n = int(input())
+# # 정수 n 입력
+# n = int(input())
 
-# 가게에 있는 부품 번호 입력
-arr1 = set(map(int, input().split()))
+# # 가게에 있는 부품 번호 입력
+# arr1 = set(map(int, input().split()))
 
-# 정수 m 입력
-m = int(input())
+# # 정수 m 입력
+# m = int(input())
 
-# 손님이 요청한 부품 번호 입력
-arr2 = list(map(int, input().split()))
+# # 손님이 요청한 부품 번호 입력
+# arr2 = list(map(int, input().split()))
 
-for i in arr2:
-    if arr1 in i:
-        print("yes")
-    else:
-        print("no")
+# for i in arr2:
+#     if arr1 in i:
+#         print("yes")
+#     else:
+#         print("no")
 
 def binary_search(array, target, start, end):
     while start <= end:
@@ -26,3 +26,24 @@ def binary_search(array, target, start, end):
         else:
             start = mid + 1
     return None
+
+# 정수 n 입력
+n = int(input())
+
+# 가게에 있는 부품 번호 입력
+arr1 = list(map(int, input().split()))
+arr1.sort()
+
+# 정수 m 입력
+m = int(input())
+
+# 손님이 요청한 부품 번호 입력
+arr2 = list(map(int, input().split()))
+
+# 요청된 부품 번호를 하나씩 확인
+for i in arr2:
+    result = binary_search(arr1, 1, 0, n - 1)
+    if result != None:
+        print("yes", end=' ')
+    else:
+        print("no", end=' ')
