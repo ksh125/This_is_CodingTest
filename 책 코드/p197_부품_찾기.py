2d2742d48be9c3644d681c02ef8ed81e65c8c253
ksh125 <rlatjdgns4238@gmail.com>
2022-05-22 19:54:50 +0900

    upload

--- 책 코드/p197_부품_찾기.py
+++ 책 코드/p197_부품_찾기.py
@@ -1,4 +1,12 @@
-def binary_search(arr, t, s, e):
+n = int(input())
+
+arr1 = list(map(int, input().split()))
+arr1.sort()
+
+m = int(input())
+arr2 = list(map(int, input().split()))
+
+def b_s(arr, t, s, e):
     while s <= e:
         mid = (s + e) // 2
         if arr[mid] == t:
@@ -8,17 +16,8 @@ def binary_search(arr, t, s, e):
         else:
             s = mid + 1
 
-n = int(input())
-
-arr1 = list(map(int, input().split()))
-arr1.sort()
-
-m = int(input())
-
-arr2 = list(map(int, input().split()))
-
 for i in arr2:
-    p = binary_search(arr1, 1, 0, n - 1)
+    p = b_s(arr1, 1, 0, n - 1)
     if p != None:
         print("yes", end=' ')
     else:
