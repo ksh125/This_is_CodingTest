5d6f41f8ec412d47ef9fb383324fe3a28c0454ad
ksh125 <rlatjdgns4238@gmail.com>
2022-05-22 18:06:29 +0900

    upload

--- 백준/s3_11663_선분_위의_점.py
+++ 백준/s3_11663_선분_위의_점.py
@@ -1,31 +1,12 @@
+from bisect import bisect_left, bisect_right
 import sys
-
-n, m = map(int, sys.stdin.readline().split())
-a = list(map(int, sys.stdin.readline().split()))
-a.sort()
-
-def na(a):
-    start = 0
-    end = n - 1
-    while start <= end:
-        mid = (start + end) // 2
-        if a[mid] < a:
-            start = mid + 1
-        else:
-            end = mid - 1
-    return end + 1
-
-def nna(b):
-    start = 0
-    end = n - 1
-    while start <= end:
-        mid = (start + end) // 2
-        if b < a[mid]:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return end
-
-for i in range(m):
-    g, h = map(int, sys.stdin.readline().split())
-    print(nna(h) - na(g) + 1)
+ 
+n, m = map(int, sys.stdin.readline().rstrip().split())
+p = list(map(int, sys.stdin.readline().rstrip().split()))
+p.sort()
+ 
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().rstrip().split())
+    lidx = bisect_left(p, a)
+    ridx = bisect_right(p, b)
+    print(ridx - lidx)
--- 책 코드/p197_부품_찾기.py
+++ 책 코드/p197_부품_찾기.py
@@ -1,49 +1,25 @@
-# # 정수 n 입력
-# n = int(input())
-
-# # 가게에 있는 부품 번호 입력
-# arr1 = set(map(int, input().split()))
-
-# # 정수 m 입력
-# m = int(input())
-
-# # 손님이 요청한 부품 번호 입력
-# arr2 = list(map(int, input().split()))
-
-# for i in arr2:
-#     if arr1 in i:
-#         print("yes")
-#     else:
-#         print("no")
-
-def binary_search(array, target, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-        if array[mid] == target:
+def binary_search(arr, t, s, e):
+    while s <= e:
+        mid = (s + e) // 2
+        if arr[mid] == t:
             return mid
-        elif array[mid] > target:
-            end = mid - 1
+        elif arr[mid] > t:
+            e = mid - 1
         else:
-            start = mid + 1
-    return None
+            s = mid + 1
 
-# 정수 n 입력
 n = int(input())
 
-# 가게에 있는 부품 번호 입력
 arr1 = list(map(int, input().split()))
 arr1.sort()
 
-# 정수 m 입력
 m = int(input())
 
-# 손님이 요청한 부품 번호 입력
 arr2 = list(map(int, input().split()))
 
-# 요청된 부품 번호를 하나씩 확인
 for i in arr2:
-    result = binary_search(arr1, 1, 0, n - 1)
-    if result != None:
+    p = binary_search(arr1, 1, 0, n - 1)
+    if p != None:
         print("yes", end=' ')
     else:
         print("no", end=' ')
