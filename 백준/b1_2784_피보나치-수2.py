aaed42e48752641fa849c74e4162ce1478c32c7a
ksh125 <rlatjdgns4238@gmail.com>
2022-05-23 21:36:26 +0900

    upload

---
+++ 백준/b1_2784_피보나치-수2.py
@@ -0,0 +1,7 @@
+def fib(n):
+    x, y = 0, 1
+    for i in range(n):
+        x, y = y, x+y
+    return x
+
+print(fib(int(input())))
--- 백준/s4_10862_피보나치-수4.py
+++ 백준/s4_10862_피보나치-수4.py
@@ -1,8 +1,9 @@
+n = int(input())
+
 def fibo(n):
     a, b = 0, 1
     for _ in range(n):
         a, b = b, a + b
     return a
 
-n = int(input())
 print(fibo(n))
